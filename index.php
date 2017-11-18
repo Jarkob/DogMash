7c2ce85bf4d533d9a2db6a112bfddaa61e459250
Jakob Bussas <jakobbu@gmx.de>
2017-11-18 08:22:55 +0100

    debug

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+echo "Test";
 
 require_once("src/config.php");
+echo "Test2";
 
 require_once("src/log.php");
 log::logAccess();
+echo "Test3";
 
 require_once("templates/templateFunctions.php");
 
