7017098a4f03033f991bf32e9ec2e3af6d20c02b
Jakob Bussas <jakobbu@gmx.de>
2019-08-15 15:52:28 +0200

    back to working mode

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-echo "hello world";
 require_once("src/config.php");
 
 require_once("src/log.php");
 log::logAccess();
 
-//require_once("templates/templateFunctions.php");
+require_once("templates/templateFunctions.php");
 
-/*require_once("templates/header.php");
+require_once("templates/header.php");
 require_once("templates/main.php");
-require_once("templates/footer.php");*/
+require_once("templates/footer.php");
 ?>
