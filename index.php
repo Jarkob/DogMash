8ae8778e4268e4486bda912df475b393473b5343
Jakob Bussas <jakobbu@gmx.de>
2019-08-15 15:31:55 +0200

    live troubleshooting

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 require_once("src/config.php");
 
-require_once("src/log.php");
-log::logAccess();
+//require_once("src/log.php");
+//log::logAccess();
 
 require_once("templates/templateFunctions.php");
 
